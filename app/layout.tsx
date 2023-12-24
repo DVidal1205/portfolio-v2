01a8364045b20e25792f024d06174fa147f04b05
Dylan Vidal <dylanvidal1204@gmail.com>
2023-12-23 22:48:55 -0500

    Adds Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -5,21 +7,22 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Dylan Vidal",
-    description: "Resume and Portfolio for Dylan Vidal, an Aspiring Software Engineer",
-    keywords: ["Developer", "Software Engineer", "Portfolio", "Dylan Vidal"],
-    icons: ["icons8-tarot-cards-doodle-32.png"],
-    generator: "Next.js",
+  title: "Dylan Vidal",
+  description:
+    "Resume and Portfolio for Dylan Vidal, an Aspiring Software Engineer",
+  keywords: ["Developer", "Software Engineer", "Portfolio", "Dylan Vidal"],
+  icons: ["icons8-tarot-cards-doodle-32.png"],
+  generator: "Next.js",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
