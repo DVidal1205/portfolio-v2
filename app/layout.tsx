1d50720d1fcf262b54ec397d0d5bb0c0749fd68b
Dylan Vidal <dylanvidal1204@gmail.com>
2024-05-13 17:59:50 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,11 @@ export default function RootLayout({
                 src="https://analytics.us.umami.is/script.js"
                 data-website-id="34db6362-d5b5-4e51-8937-dc4c7b7a5006"
             />
+            <Script
+                async
+                src="https://dormdevs-analytics.vercel.app/script.js"
+                data-website-id="c309e430-da82-4161-b0d0-08714de60cda"
+            />
             <body>{children}</body>
             <Analytics />
         </html>
